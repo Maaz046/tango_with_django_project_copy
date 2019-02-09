a155e301f28be0f3390673901cfcfe30f2933cd8
Maaz Tariq <mzed046@gmail.com>
2019-02-09 19:45:30 +0000

    Chapter 9

--- tango_with_django_project/rango/models.py
+++ tango_with_django_project/rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -24,9 +25,12 @@ class Page(models.Model):
 	url = models.URLField()
 	views = models.IntegerField(default=0)
 
-	def save(self, *args, **kwargs):
-		self.slug = slugify(self.title)
-		super(Page, self).save(*args, **kwargs)
-
 	def __str__(self):
 		return self.title
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	website = models.URLField(blank=True)
+	picture = models.ImageField(upload_to='profile_images', blank=True)
+	def __str__(self):
+		return self.user.username
